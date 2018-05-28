f586d7441931e13eff97d1d868cc7480cd011934
Peter Rogers <peter.rogers@gmai.com>
2018-05-28 11:33:04 -0400

    Added basic photo uploading code

--- photo/urls.py
+++ photo/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    #path('hello/', views.hello, name='hello'),
+    path('upload', views.upload, name='upload'),
     #path('<int:test_id>', views.detail, name='detail'),
 ]
