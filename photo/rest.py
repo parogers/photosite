260c95914f0cce1f1bbeb48164c692c0530131b7
Peter Rogers <peter.rogers@gmai.com>
2018-06-04 12:07:43 -0400

    Added basic authentication to rest api

--- photo/rest.py
+++ photo/rest.py
@@ -1,6 +1,8 @@
 
 from .models import Photo
-from rest_framework import serializers, viewsets
+from rest_framework import serializers, viewsets, permissions
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 class PhotoSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +13,12 @@ class PhotoSerializer(serializers.ModelSerializer):
 class PhotoViewSet(viewsets.ModelViewSet):
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
+
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    # detail=False => this action can be called without referring to a photo
+    # object (eg GET /api/photo/something)
+    @action(detail=False, methods=['get'])
+    def something(self, request):
+        return Response('hello world')
+
