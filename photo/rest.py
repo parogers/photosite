235f044c43b82316498621ddaf057c1b07ba7fe3
Peter Rogers <peter.rogers@gmail.com>
2018-06-27 13:23:59 -0400

    Added photo uploading code niceness with javascript

--- photo/rest.py
+++ photo/rest.py
@@ -1,3 +1,18 @@
+# photosite - A minimalist django site for posting photos
+# Copyright (C) 2018  Peter Rogers (peter.rogers@gmail.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from .models import Photo
 from rest_framework import serializers, viewsets, permissions
@@ -7,9 +22,11 @@ from rest_framework.decorators import action
 class PhotoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Photo
-        fields = ('image', 'image_width', 'image_height')
+        fields = ('id', 'image', 'image_width', 'image_height')
+        # Fields that are ignored during record updates
         read_only_fields = ('image_width', 'image_height')
 
+# Provides CRUD operations for photo records
 class PhotoViewSet(viewsets.ModelViewSet):
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
@@ -21,4 +38,3 @@ class PhotoViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=['get'])
     def something(self, request):
         return Response('hello world')
-
