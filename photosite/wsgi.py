fc872139d46d86ee8c3aa0afabbed3860a23fe58
Peter Rogers <peter.rogers@gmai.com>
2018-09-01 14:05:50 -0400

    Added WSGI debug wrapper class

--- photosite/wsgi.py
+++ photosite/wsgi.py
@@ -13,4 +13,26 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "photosite.settings")
 
+class DebugWrapper:
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        # WSGI interface is defined in PEP-3333
+        # https://www.python.org/dev/peps/pep-3333/
+        print(environ['REQUEST_METHOD'], environ['PATH_INFO'])
+        print('Query string:', environ['QUERY_STRING'])
+        print('Streams:')
+        print('* wsgi.input', environ['wsgi.input'])
+        print('* wsgi.errors', environ['wsgi.errors'])
+        print('Headers:')
+        http_headers = filter(
+            lambda x : x.startswith('HTTP_'),
+            environ.keys())
+        for header in http_headers:
+            print('*', header, environ[header])
+        
+        return self.app(environ, start_response)
+
 application = get_wsgi_application()
+#application = DebugWrapper(get_wsgi_application())
